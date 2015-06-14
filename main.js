dcf74c47835f7944b37bf876dff15e8a99819d0a
skaplun <shai.kaplun@gmail.com>
2015-06-14 15:26:37 +0000

    add main.js

--- main.js
+++ main.js
@@ -3,77 +3,37 @@ document.addEventListener('DOMContentLoaded', function(){
 
 
 var MixinFac = (function() {
+    var htmlSnippets = {
+        dragHandler : crel('div', {'class': 'rd_m_draghandler'}),
+        delbutton : crel('div', {'class': 'rd_m_del'}, 'X')
+    }
     
     function interactable(elem){
         
-        function dragMoveListener (event) {
-            var target = event.target,
-                // keep the dragged position in the data-x/data-y attributes
-                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-            // translate the element
-            target.style.webkitTransform =
-            target.style.transform =
-              'translate(' + x + 'px, ' + y + 'px)';
-
-            // update the posiion attributes
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
+        function setResizeConstants(){
+            console.log(arguments)
+            console.log(this)
+            //  this.initFontSize = parseFloat(this.alsoResize.css('fontSize'), 10);
+            //  this.initWidth = this.html[0].clientWidth;
+        }
+        function resizeText(e, uiObj){
+            return this.alsoResize.css('fontSize', this.initFontSize * (uiObj.size.width / this.initWidth))
         }
         
-        elem.interactable = interact(elem.html)
-            .draggable({
-        // enable inertial throwing
-        inertia: true,
-        // keep the element within the area of it's parent
-        restrict: {
-          restriction: "#container",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        },
-
-        // call this function on every dragmove event
-        onmove: dragMoveListener,
-        // call this function on every dragend event
-        onend: function (event) {
-          var textEl = event.target.querySelector('p');
-
-          textEl && (textEl.textContent =
-            'moved a distance of '
-            + (Math.sqrt(event.dx * event.dx +
-                         event.dy * event.dy)|0) + 'px');
-        }
-        })
+        elem.html.appendChild(htmlSnippets.dragHandler.cloneNode(true));
+        elem.html.appendChild(htmlSnippets.delbutton.cloneNode(true));
+        console.log(elem.html.innerHTML)
+        elem.$html
             .resizable({
-    edges: { left: true, right: true, bottom: true, top: true }
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = event.rect.width + '×' + event.rect.height;
-  });
-        
-//        .resizable({
-//                restrict: {
-//                    restriction: 'parent',
-//                }
-//            })
+                create : setResizeConstants,
+                resize : resizeText,
+                alsoResize: '.rd_m_draghandler',
+            })
+            .draggable({
+                handle: '.rd_m_draghandler',
+                 containment: 'parent'
+                 
+            })
 
         return elem
         console.log(elem)    
@@ -99,7 +59,7 @@ var MixinFac = (function() {
         
     }
         
-    mixins = {
+    var mixins = {
         'button' : [button, interactable]
     
     }
