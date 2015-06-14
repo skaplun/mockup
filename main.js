16270220283e2a8d400a25a3f43d98c1429d5c66
skaplun <shai.kaplun@gmail.com>
2015-06-14 07:31:50 +0000

    add ui, structure

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ var MixinFac = (function() {
         inertia: true,
         // keep the element within the area of it's parent
         restrict: {
-          restriction: "parent",
+          restriction: "#container",
           endOnly: true,
           elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
         },
